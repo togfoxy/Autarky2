433d459d28b1a4b035fe473fe016f5bba34a98a8
Fox <g_t_campbell@yahoo.com.au>
2022-10-27 22:46:43 +1000

    Got the movement and timer right

--- Autarky2/people.lua
+++ Autarky2/people.lua
@@ -8,19 +8,16 @@ function people.initialise()
         PERSONS[i] = {}
         PERSONS[i].row = love.math.random(1, NUMBER_OF_ROWS)
         PERSONS[i].col = love.math.random(1, NUMBER_OF_COLS)
-        PERSONS[i].destrow = nil
-        PERSONS[i].destcol = nil
-        PERSONS[i].destx = nil
-        PERSONS[i].desty = nil
+        PERSONS[i].destrow = PERSONS[i].row
+        PERSONS[i].destcol = PERSONS[i].col
 
         -- add a bit of a border to ensure the person stays inside the tile
         local min = 0 + (PERSONS_RADIUS * 2)
         local max = TILE_SIZE - (PERSONS_RADIUS * 2)
         PERSONS[i].x = love.math.random(min, max)
         PERSONS[i].y = love.math.random(min, max)
-        PERSONS[i].destx = nil
-        PERSONS[i].desty = nil
-
+        PERSONS[i].destx = PERSONS[i].x
+        PERSONS[i].desty = PERSONS[i].y
     end
 end
 
@@ -37,6 +34,14 @@ function people.draw()
         love.graphics.circle("fill", drawx, drawy, PERSONS_RADIUS)
 
         -- print(drawx, drawy, person.col, person.row, person.x, person.y, TILE_SIZE, TOP_MARGIN)
+
+        -- destination
+        -- if person.destrow ~= nil and person.destcol ~= nil then
+        --     drawy = ((person.destrow - 1) * TILE_SIZE) + person.desty + TOP_MARGIN
+        --     drawx = ((person.destcol - 1) * TILE_SIZE) + person.destx + LEFT_MARGIN
+        --     love.graphics.setColor(1,0,0,0.5)
+        --     love.graphics.circle("line", drawx, drawy, PERSONS_RADIUS)
+        -- end
     end
 end
 
@@ -48,6 +53,7 @@ function people.assignDestination(hour)
             --! currently random
             person.destrow = love.math.random(1, NUMBER_OF_ROWS)
             person.destcol = love.math.random(1, NUMBER_OF_COLS)
+
             local min = 0 + (PERSONS_RADIUS * 2)
             local max = TILE_SIZE - (PERSONS_RADIUS * 2)
             person.destx = love.math.random(min, max)
@@ -62,8 +68,10 @@ function people.moveToDestination(dt)
     local result = false        -- return true if at least one person moved
 
     for k, person in pairs(PERSONS) do
-        if person.destx ~= nil and person.desty ~= nil then
-            result = true
+        if person.row ~= person.destrow or person.col ~= person.destcol or
+            person.x ~= person.destx or person.y ~= person.desty then
+
+            result = true   -- true = there is movement
 
             -- get absolute screen coordinates
             local currentx = ((person.col - 1) * TILE_SIZE) + person.x + LEFT_MARGIN
@@ -71,42 +79,52 @@ function people.moveToDestination(dt)
             local screenx = ((person.destcol - 1) * TILE_SIZE) + person.destx + LEFT_MARGIN
             local screeny = ((person.destrow - 1) * TILE_SIZE) + person.desty + LEFT_MARGIN
 
-            if currentx ~= screenx then -- or currenty ~= screeny then
-                -- move towards destination
-                -- move right
-                if screenx > currentx then
-                    local disttodest = screenx - currentx
-                    local distdt = MOVEMENT_RATE * dt
-                    local adjustment = math.min(disttodest, distdt)
-                    person.x = person.x + adjustment
-                    person.x = cf.round(person.x)
-                    if person.x > TILE_SIZE then
-                        person.col = person.col + 1
-                        person.x = 1
-                    end
-                    -- recalc current x
-                    currentx = ((person.col - 1) * TILE_SIZE) + person.x + LEFT_MARGIN
-                    if currentx == screenx then
-                        person.destx = nil
-                    end
-                elseif screenx < currentx then
-                    -- move left
-                    local disttodest = currentx - screenx
-                    local distdt = MOVEMENT_RATE * dt
-                    local adjustment = math.min(disttodest, distdt)
-                    person.x = person.x - adjustment
-                    person.x = cf.round(person.x)
-                    if person.x < 0 then
-                        person.col = person.col - 1
-                        person.x = TILE_SIZE
-                    end
-                    -- recalc current x
-                    currentx = ((person.col - 1) * TILE_SIZE) + person.x + LEFT_MARGIN
-                    if currentx == screenx then
-                        person.destx = nil
-                    end
+            -- move right
+            if screenx > currentx then
+                local disttodest = screenx - currentx
+                local distdt = MOVEMENT_RATE * dt
+                local adjustment = math.min(disttodest, distdt)
+                person.x = person.x + adjustment
+                person.x = cf.round(person.x)
+                if person.x > TILE_SIZE then
+                    person.col = person.col + 1
+                    person.x = 1
                 end
+            elseif screenx < currentx then
+                -- move left
+                local disttodest = currentx - screenx
+                local distdt = MOVEMENT_RATE * dt
+                local adjustment = math.min(disttodest, distdt)
+                person.x = person.x - adjustment
+                person.x = cf.round(person.x)
+                if person.x < 0 then
+                    person.col = person.col - 1
+                    person.x = TILE_SIZE
+                end
+            end
 
+            -- move down
+            if screeny > currenty then
+                local disttodest = screeny - currenty
+                local distdt = MOVEMENT_RATE * dt
+                local adjustment = math.min(disttodest, distdt)
+                person.y = person.y + adjustment
+                person.y = cf.round(person.y)
+                if person.y > TILE_SIZE then
+                    person.row = person.row + 1
+                    person.y = 1
+                end
+            elseif screeny < currenty then
+                -- move up
+                local disttodest = currenty - screeny
+                local distdt = MOVEMENT_RATE * dt
+                local adjustment = math.min(disttodest, distdt)
+                person.y = person.y - adjustment
+                person.y = cf.round(person.y)
+                if person.y < 0 then
+                    person.row = person.row - 1
+                    person.y = TILE_SIZE
+                end
             end
         end
     end
